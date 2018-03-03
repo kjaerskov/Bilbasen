f4be17803e2c0859ccb9e8122632df2a3fb6f692
kjaerskov <36921678+kjaerskov@users.noreply.github.com>
2018-03-03 18:59:40 +0100

    reorganising files

--- CrawlBilbasen_RandomForest.R
+++ CrawlBilbasen_RandomForest.R
@@ -3,70 +3,9 @@ library('ggplot2') # visualization
 library('scales') # visualization
 library('dplyr') # data manipulation
 library('randomForest') # classification algorithm
-library('rvest') # web scraper
 
-#Clean up
-rm(list=ls())
-closeAllConnections()
-
-#Get data from URL
-URLtoCrawl <- "https://www.bilbasen.dk/brugt/bil/vw?fuel=0&yearfrom=2010&yearto=2018&pricefrom=0&priceto=10000000&mileagefrom=-1&mileageto=10000001&zipcode=0000&includeengroscvr=false&includesellforcustomer=true&includewithoutvehicleregistrationtax=false&includeleasing=false&page=1"
-website <- read_html(URLtoCrawl)
-NumberOfHits <- website %>% html_nodes(".scrambledtab") %>% html_text(trim = TRUE)
-NumberOfHits <- as.numeric(gsub(".+\\((\\d+).","\\1",gsub("\\.","",NumberOfHits[1])))
-
-#Initialize
-CarmakeDetail <- NULL
-CarURL <- NULL
-CarConsumption <- NULL
-CarMileage <- NULL
-CarYear <- NULL
-CarRegion <- NULL
-CarPrice <- NULL
-
-#Cookie set to display 32 results per page
-NumberOfPages <- ceiling(NumberOfHits/32)
-
-for(i in 1:NumberOfPages) {
-  
-  # Set URL to equal increasing page number denoted by i
-  URLtoCrawl <- gsub(paste("page=",i-1,sep=""),paste("page=",i,sep=""),URLtoCrawl)
-  website <- read_html(URLtoCrawl)
-  
-  #Detailed description of car make
-  CarmakeDetail <- c(CarmakeDetail, website %>% html_nodes(".darkLink") %>% html_text(trim = TRUE))
-  CarURL <- c(CarURL, website %>% html_nodes(".darkLink") %>% html_attr("href"))
-  
-  #Car price
-  CarPrice <- c(CarPrice, website %>% html_nodes(".listing-price")  %>% html_text(trim = TRUE))
-  
-  #Car region
-  CarRegion <- c(CarRegion, website %>% html_nodes(".listing-region")  %>% html_text(trim = TRUE))
-  
-  #Car metrics
-  CarListingData <- website %>% html_nodes(".listing-data")  %>% html_text(trim = TRUE)
-  CarConsumption <- c(CarConsumption, CarListingData[1:4==2])
-  CarMileage <- c(CarMileage, CarListingData[1:4==3])
-  CarYear <- c(CarYear, CarListingData[1:4==4])
-}
-
-
-#Create dataframe
-df = data.frame(CarmakeDetail,
-                URL = CarURL,
-                Year = CarYear,
-                Model = factor(gsub("^\\w+{1}[[:blank:]](.+)[[:blank:]]\\d,.*$","\\1",CarmakeDetail)),
-                MakeModel = gsub("^(.+)[[:blank:]]\\d,.*$","\\1",CarmakeDetail),
-                Region = CarRegion, 
-                Make = factor(gsub("^((\\w+){1}\\w+).*$","\\1",CarmakeDetail)),
-                Engine = as.numeric(gsub(",",".",gsub("^.+[[:blank:]](\\d,\\d+).*$","\\1",CarmakeDetail))),
-                Doors = gsub("^.+[[:blank:]](\\w{2,})$","\\1",CarmakeDetail),
-                Horsepower = as.numeric(gsub("^.+[[:blank:]](\\d{2,}).*$","\\1",CarmakeDetail)),
-                YearNumeric = as.numeric(CarYear),
-                Price = as.numeric(gsub("\\.","",gsub(" kr.", "", CarPrice))),
-                Mileage = as.numeric(gsub("\\.","",CarMileage)),
-                Consumption = as.numeric(gsub(",",".",(gsub(".*?([0-9]+,[0-9]).*", "\\1", CarConsumption))))
-                )
+# Source the web scraper for bilbasen.dk
+source("Crawlbilbasen.R")
 
 #Remove rows with n.a. data
 df <- na.omit(df)
